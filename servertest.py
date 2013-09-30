53a7ec9c6ead3fcb5db1378e2f8ecc5b89a1be7b
Mark Kampe <mark.kampe@gmail.com>
2013-09-29 21:47:40 -0700

    performance models: refactor reporting code 	create one common report generation class 	change existing test cases to use it

--- servertest.py
+++ servertest.py
@@ -6,27 +6,27 @@
 exerciser for a single server
 """
 
+from report import Report
 from units import *
 
 
-def servertest(fs, obj_size=4 * MEG, nobj=2500, depth=1, crtdlt=False):
+def servertest(fs, obj_size=4 * MEG, depth=1, crtdlt=False):
     """ compute & display standard test results """
 
     if crtdlt:
         tc = fs.create()
         td = fs.delete()
-        print("\t\t     create\t      delete")
-        print("\t\t%6d IOPS\t %6d IOPS" % (iops(tc), iops(td)))
-        print("")
 
-    print("\t    bs\t    rnd read\t   rnd write")
-    print("\t -----\t    --------\t   ---------")
+        r = Report(("create", "delete"))
+        r.printHeading()
+        r.printIOPS(1, (SECOND / tc, SECOND / td))
+        r.printLatency(1, (tc, td))
+
+    r = Report(("rnd read", "rnd write"))
+    r.printHeading()
     for bs in (4096, 128 * 1024, 4096 * 1024):
-        (trr,brr) = fs.read(bs, depth=depth)
-        (trw,brw) = fs.write(bs, depth=depth)
-
-        format = "\t%5dK\t%7.1f MB/s\t%7.1f MB/s"
-        print(format %
-              (kb(bs), meg(float(brr)), meg(float(brw))))
-        print("\t    \t %6d IOPS\t %6d IOPS" % (brr/bs, brw/bs))
-        print("\t    \t %6d us\t %6d us\t(average latency)" % (trr, trw))
+        (trr, brr) = fs.read(bs, depth=depth)
+        (trw, brw) = fs.write(bs, depth=depth)
+        r.printBW(bs, (brr, brw))
+        r.printIOPS(bs, (brr, brw))
+        r.printLatency(bs, (trr, trw))
